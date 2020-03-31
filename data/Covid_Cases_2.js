7549d4b6334c816feccd09d4021c68e11603aa8a
jcadag <62292076+jcadag@users.noreply.github.com>
2020-03-31 11:46:24 +0800

    Add files via upload

---
+++ data/Covid_Cases_2.js
@@ -0,0 +1,18 @@
+var json_Covid_Cases_2 = {
+"type": "FeatureCollection",
+"name": "Covid_Cases_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "Confirmed" }, "geometry": { "type": "Point", "coordinates": [ 118.74943061648564, 9.751116746065282 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "Confirmed" }, "geometry": { "type": "Point", "coordinates": [ 118.758428474350524, 9.772160713042972 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "Confirmed" }, "geometry": { "type": "Point", "coordinates": [ 118.725122970611821, 9.789762505897391 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "Confirmed" }, "geometry": { "type": "Point", "coordinates": [ 118.688057168063921, 9.745293023571532 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "Confirmed" }, "geometry": { "type": "Point", "coordinates": [ 118.7632631442481, 9.732189276333814 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUM" }, "geometry": { "type": "Point", "coordinates": [ 118.754936768313399, 9.787380362922159 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUM" }, "geometry": { "type": "Point", "coordinates": [ 118.742178611639318, 9.767528507427722 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUM" }, "geometry": { "type": "Point", "coordinates": [ 118.76366603340621, 9.763293291589587 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUI" }, "geometry": { "type": "Point", "coordinates": [ 118.713036295867937, 9.784468831639961 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUI" }, "geometry": { "type": "Point", "coordinates": [ 118.727674601946646, 9.772822451437733 ] } },
+{ "type": "Feature", "properties": { "id": null, "COVID_Case": "PUI" }, "geometry": { "type": "Point", "coordinates": [ 118.766754850285224, 9.793865045404836 ] } }
+]
+}
